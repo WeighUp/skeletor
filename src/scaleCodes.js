504a7c32dbf15e5deb77d2529edc029cd227c5f7
speakingcode <dlissner@gmail.com>
2019-07-02 13:50:53 -0500

    Fix scalecodes

---
+++ src/scaleCodes.js
@@ -0,0 +1,36 @@
+export const CMSG_ERROR = 0;
+export const CMSG_I_AM = 1;
+export const CMSG_LIFT = 2;
+export const CMSG_REZERO = 3;
+export const CMSG_REPLACE = 4;
+export const CMSG_TARE = 5;
+export const CMSG_SCALE = 6;
+export const CMSG_CURWEIGHT = 7;
+export const CMSG_MEAS = 8;
+export const CMSG_WR_FLSH = 9;
+export const CMSG_AUTOWGT = 0x0A;
+export const CMSG_AUTOZERO = 0x0B;
+export const CMSG_SETZERO = 0x0C;
+export const CMSG_SETSCALE = 0x0D;
+export const CMSG_GET_TEMP = 0x0E;
+
+export const CCMD_RESET = 0x80;
+export const CCMD_IDENTIFY = 0x81;
+export const CCMD_YOU_ARE = 0x82;
+export const CCMD_SET_SERIAL = 0x83;
+export const CCMD_TARE = 0x84;
+export const CCMD_SCALE = 0x85;
+export const CCMD_MEAS = 0x86;
+export const CCMD_WR_FLSH = 0x87;
+export const CCMD_AUTOWGT = 0x88;
+export const CCMD_AUTOZERO = 0x89;
+export const CCMD_SETZERO = 0x8A;
+export const CCMD_SETSCALE = 0x8B;
+export const CCMD_GET_TEMP = 0x8C;
+export const CCMD_MAX = 0x8D;
+
+export const EXTD_MSG = 0xAAE8
+export const PADDING  = 0x00
+export const TERM     = 0x55
+export const NO_ERROR = 0x00
+
