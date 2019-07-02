79c4be08215b0aaa2b8ef8e28f7901bd4bd50321
speakingcode <dlissner@gmail.com>
2019-07-02 14:23:08 -0500

    Post measurement via axios

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import SerialPort from 'serialport'
 
 import * as ScaleCodes from './scaleCodes'
 
-const port = new SerialPort('/dev/ttyUSB0', {baudRate: 1228800})
+const WEIGHUP_SCALE_DEVICE_PATH = process.env.WEIGHUP_SCALE_DEVICE_PATH
+const WEIGHUP_API_URL           = process.env.WEIGHUP_API_URL
+const WEIGHUP_HUB_ID            = process.env.WEIGHUP_HUB_ID
+
+const port = new SerialPort(WEIGHUP_SCALE_DEVICE_PATH, {baudRate: 1228800})
+
 
 port.on('data', serialData => {
   const messagePrefix  = serialData.slice(0,2),
@@ -21,6 +26,17 @@ port.on('data', serialData => {
     address: ${address}
     payload: ${payload}
   `)
+
+    axios.post(`${WEIGHUP_API_URL}measurements.json`,{
+      hub_id: WEIGHUP_HUB_ID,
+      scale_id: 1,
+      weight: message.payload.slice(0,4)[0] > 0 ? message.payload.slice(0,4)[0] : 0,
+      //reading_time: moment().format("ddd MMM DD HH:mm:ss ZZ YYYY")
+      reading_time: moment().format("YYYY-MMM-DD HH:mm:ss")
+    })
+    .catch(error => {
+      console.log(error)
+    })
 
 
 })
