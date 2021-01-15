7e2a59bae7718633cd19fbd535f4d6311b2fb1a1
speakingcode <dlissner@gmail.com>
2021-01-15 10:17:51 -0600

    More configuation stuff

--- .env.ex
+++ .env.ex
@@ -1,4 +1,4 @@
-WEIGHUP_SCALE_DEVICE_PATH = '/dev/ttyUSB0';
+WEIGHUP_SCALE_DEVICE_PATH = '/dev/ttyUSB0'
 WEIGHUP_API_URL           = 'http://localhost:8000/api/v1'
 WEIGHUP_HUB_ID            = 1
 WEIGHUP_SCALE_INTERVAL    = 500
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const WEIGHUP_SCALE_DEVICE_PATH = process.env.WEIGHUP_SCALE_DEVICE_PATH || '/dev/ttyUSB0';
+export const WEIGHUP_SCALE_DEVICE_PATH = process.env.WEIGHUP_SCALE_DEVICE_PATH || '/dev/ttyUSB0'
 export const WEIGHUP_API_URL           = process.env.WEIGHUP_API_URL || 'http://localhost:8000/api/v1'
 export const WEIGHUP_HUB_ID            = parseInt(process.env.WEIGHUP_HUB_ID) || 1
 export const WEIGHUP_SCALE_INTERVAL    = parseInt(process.env.WEIGHUP_SCALE_INTERVAL) || 500
